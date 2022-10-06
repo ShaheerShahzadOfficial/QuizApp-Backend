01b8b6ce25e770a1c9c0a342d4de5fcaccdc0d51
SHAHEERSHAHZADofficial <mohammedshaheershahzadrawda5@gmail.com>
2022-10-06 15:35:38 +0500

    vercel

--- server.js
+++ server.js
@@ -2,21 +2,15 @@ import http from "http"
 import app from "./app.js";
 import dotenv, { config } from "dotenv"
 import DBConnection from "./config/database/database.js"
-// import cloudinary from "cloudinary"
 const Server = http.createServer(app)
 
 // config
 dotenv.config({path:"config/config.env"})
 
-// //// Data Base Connection
 
 DBConnection()
 
-// cloudinary.config({
-//     cloud_name: process.env.CLOUDINARY_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,
-// })
+
 
 const port = process.env.PORT || 4000
 Server.listen(port, () => {
